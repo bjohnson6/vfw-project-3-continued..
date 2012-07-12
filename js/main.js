8972ea25013651f83233ab454e8fb6250cbece13
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-07-12 19:04:37 -0400

    Set up my javascript set links

--- js/main.js
+++ js/main.js
@@ -2,22 +2,48 @@
 //Project 1
 // Visual Frameworks 12/06
 
+
+//wait until the dom is ready.
 window.addEventListener("DOMContentLoaded", function(){	   
 
 
 
 //getelementByID function
-function elementName(x){
-         var elementName = document.getElementById(x);
-         return elementName;              
-    };
+
+
+function $(x){
+    var theElement = document.getElementById(x);
+    return theElement;              
+};
 
 
 
 
 //Variable defaults
+var addAnEvent =[
+   "--Choose Your Moment--", 
+   "First Met", 
+   "First Date",
+   "First Kiss",
+   "First Said I Love You!", 
+   "Our Song",
+   "--Choose Something Else--",
+   "Our Names",
+   "Relationship Status",
+   "Calculate Time Together",
+   "Note A Special Moment"
+]
 
 
 
 
 //Set Link and Submit click events
+var displayLink = $("displayLink");
+displayLink.addEventListener("click",getData);
+var clearLink=$("clear");
+clearLink.addEventListener("click",clearLocal);
+var save = $("submit");
+save.addEventListener("click",storeData);
+
+
+});
