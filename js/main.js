8c0fcaa3aaa9e00281449ceb371de4ba27080d98
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-07-12 16:15:58 -0400

    Started my javascript file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 //Bernice Johnson
 //Project 1
 // Visual Frameworks 12/06
+
+window.addEventListener("DOMContentLoaded", function(){	   
+
+
+
+//getelementByID function
+function elementName(x){
+         var elementName = document.getElementById(x);
+         return elementName;              
+    };
+
+
+
+
+//Variable defaults
+
+
+
+
+//Set Link and Submit click events
